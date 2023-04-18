acd0bfd38266c463f2f5a4654a4ec21c43633a75
DuMark Allman <dumark.allman@yahoo.com>
2023-04-18 15:55:36 -0400

    [StewieDuey]

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-console.log('%c HI', 'color: firebrick')
+let breeds =[]
+
+function getBreeds() { 
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    return fetch(imgUrl) 
+    .then(response => response.json())
+    .then (response => {
+        //console.log("response", response.message)
+        const dogImageContainer = document.getElementById("dog-image-container")
+        response.message.forEach(url => {
+            const img  = document.createElement("img")
+            img.src = url
+            dogImageContainer.appendChild(img)
+        })
+    })
+}
+
+function getDogBreedNames() {
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(response => {
+       breeds = Object.keys(response.message)
+       addBreedNamesToDom(breeds)
+       })
+}
+
+
+function addBreedNamesToDom(breeds){
+  const ul = document.querySelector("#dog-breeds")
+  breeds.map(breed => {
+    const li = document.createElement("li")
+    li.textContent = breed
+    ul.append(li)
+   }) 
+}
+
+document.addEventListener("click", event => {
+if (event.target.matches("li")) {
+    event.target.style.color = "red"
+}
+})
+
+document.addEventListener("change", event => {
+    if(event.target.matches("#breed-dropdown")) {
+        const ul = document.querySelector["#dog-breeds"]
+         ul.innerHTML = ""
+         const filterBreeds = breeds.filter (breed => breed[0] === event.target.value)
+        addBreedNamesToDom(filterBreeds)
+
+
+    }     
+})
+
+
+getBreeds();
+getDogBreedNames();
+// console.log('%c HI', 'color: firebrick')
+
